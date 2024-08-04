62ef8b8b19ef80f1be9e48d3a5e174a3890ecfc7
Zain Javaid <zain.javed.965@gmail.com>
2024-08-05 00:49:52 +0500

    spacing in about

--- components/about.tsx
+++ components/about.tsx
@@ -19,16 +19,15 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-  I started my programming journey during the COVID lockdown, where I picked up {" "}
-  <span className="font-bold italic">Python</span> and began exploring web scraping, automation, and data visualization.
-  After completing a Udemy bootcamp, I became familiar with web development and its technologies. 
-  <br></br>
-  I was fortunate to start my frontend career with an {" "}
-  <span className="font-bold italic underline">SEO team</span>, where I learned invaluable SEO standards for <span className="font-bold italic underline">creating and designing web pages.</span>  
-  {" "}Then, I transitioned to backend development, and I am now currently working as a {" "}
-  <span className="font-bold italic underline"> full time backend developer</span> while also helping people with their ideas and execution.
-</p>
-
+    I started my programming journey during the COVID lockdown, where I picked up {" "}
+    <span className="font-bold italic">Python</span> and began exploring web scraping, automation, and data visualization.
+    After completing a Udemy bootcamp, I became familiar with web development and its technologies. 
+    <br></br><br></br>
+    I was fortunate to start my frontend career with an {" "}
+    <span className="font-bold italic underline">SEO team</span>, where I learned invaluable SEO standards for <span className="font-bold italic underline">creating and designing web pages.</span>  
+    {" "}Then, I transitioned to backend development, and I am now currently working as a {" "}
+    <span className="font-bold italic underline"> full time backend developer</span> while also helping people with their ideas and execution.
+    </p>
 
       <p>
         <span className="italic">When I'm not coding</span>,
